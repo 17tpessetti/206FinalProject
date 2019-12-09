23d64434032261a09c557e40c8db92c66645536e
Taylor <tmpess@umich.edu>
2019-12-08 22:24:59 -0500

    spotipy is done

--- length_calculations.py
+++ length_calculations.py
@@ -1,8 +1,11 @@
 import spotipy
 import requests
 import json
+import csv
 import sqlite3
+import matplotlib.pyplot as plt
 import os
+import numpy as np
 import spotipy.util as util
 from spotipy.oauth2 import SpotifyClientCredentials #To access authorised Spotify data
 
@@ -11,8 +14,9 @@ path = os.path.dirname(os.path.abspath(__file__))
 conn = sqlite3.connect(path+'/'+db)
 cur = conn.cursor()
 
-def get_lengths_from_db():
-    cur.execute('SELECT length FROM Spotipy')
+def get_data_from_db():
+    #getting the length of each song and writing it to a csv file
+    cur.execute('SELECT Length FROM Spotipy')
     result = cur.fetchall()
     shortest = 0
     two45 = 0
@@ -45,5 +49,106 @@ def get_lengths_from_db():
             fourmin15 += 1
         else:
             fourmin30 += 1
-    print(shortest, two45, lessthan3, threemin15, threemin30, threemin45, fourmin, fourmin15, fourmin30)
-get_lengths_from_db()
+    with open('spotipycalculations.csv', 'w') as fil:
+        writer = csv.writer(fil)
+        writer.writerow(['Lengths of songs (in minutes)', 'Number of songs'])
+        writer.writerow(['Shortest than 2:30', shortest])
+        writer.writerow(['Between 2:30 and 2:45', two45])
+        writer.writerow(['Between 2:45 and 3:00', lessthan3])
+        writer.writerow(['Between 3:00 and 3:15', threemin15])
+        writer.writerow(['Between 3:15 and 3:30', threemin30])
+        writer.writerow(['Between 3:30 and 3:45', threemin45])
+        writer.writerow(['Between 3:45 and 4:00', fourmin])
+        writer.writerow(['Between 4:00 and 4:15', fourmin15])
+        writer.writerow(['Longer than 4:15', fourmin30])
+
+    #getting the length of each song name and writing it to a csv file
+    cur.execute('SELECT Song_Name from Spotipy')
+    result2 = cur.fetchall()
+    one = 0
+    two = 0
+    three = 0
+    four = 0
+    morethan4 = 0
+    for song in result2:
+        if '(' in song[0]:
+            song_title = song[0].split('(')
+            title_split = song_title[0].split()
+        elif '-' in song[0]:
+            song_title = song[0].split('-')
+            title_split = song_title[0].split()
+        else:
+            title_split = song[0].split()
+
+        if len(title_split) == 1:
+            one += 1
+        elif len(title_split) == 2:
+            two += 1
+        elif len(title_split) == 3:
+            three += 1
+        elif len(title_split) == 4:
+            four += 1
+        else:
+            morethan4 += 1
+    with open('spotipycalculations2.csv', 'w') as fil2:
+        writer = csv.writer(fil2)
+        writer.writerow(['Lengths of song name (in words)', 'Number of songs', 'Percentage'])
+        writer.writerow(['One', one, one/100])
+        writer.writerow(['Two', two, two/100])
+        writer.writerow(['Three', three, three/100])
+        writer.writerow(['Four', four, four/100])
+        writer.writerow(['More than four', morethan4, morethan4/100])
+
+
+def make_lengths_graph():
+    x_values = []
+    y_values = []
+    with open('spotipycalculations.csv') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        count = 0
+        for row in csv_reader:
+            if count == 0:
+                count += 1
+                continue
+            else:
+                x_values.append(row[0])
+                y_values.append(float(row[1]))
+    plt.bar(x_values, y_values, align='center', color=('red', 'orange', 'yellow', 'green', 'blue', 'purple', 'violet', 'pink', 'maroon'))
+    plt.xlabel("Song Length")
+    plt.xticks(rotation=90)
+    plt.ylabel("Count of Songs per Length")
+    plt.title("Number of Songs on Spotify Top 100 by Length")
+    plt.savefig("spotify_bargraph.png")
+    plt.tight_layout()
+    plt.show()
+    return
+
+def make_song_title_graph():
+    labels = []
+    sizes = []
+    with open('spotipycalculations2.csv') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        count = 0
+        for row in csv_reader:
+            if count == 0:
+                count += 1
+                continue
+            else:
+                if row[0] == 'One':
+                    labels.append(row[0] + ' word')
+                else:
+                    labels.append(row[0] + ' words')
+                sizes.append(row[1])
+    plt.pie(sizes, labels = labels, colors = ['teal', 'purple', 'grey', 'lightcoral', 'lightskyblue'], autopct='%1.0f%%')
+    plt.axis('equal')
+    plt.title("Length of Song Names on Spotify Top 100")
+    plt.savefig("spotify_piechart.png")
+    plt.show()
+
+    
+    return
+    
+
+get_data_from_db()
+make_lengths_graph()
+make_song_title_graph()
--- spotify_api.py
+++ spotify_api.py
@@ -19,8 +19,9 @@ user = spotify.current_user()
 displayName = user['display_name']
 
 #creating the database
-def create_database():
-    cur.execute('CREATE TABLE IF NOT EXISTS Spotipy(Song_Name TEXT, Artist TEXT, Length FLOAT, Spotify_Id TEXT)')
+def create_databases():
+    cur.execute('CREATE TABLE IF NOT EXISTS Spotipy(Song_Name TEXT, Artist TEXT, Length FLOAT)')
+    cur.execute('CREATE TABLE IF NOT EXISTS Spotipy_Ids(Song_Name TEXT, Spotify_Id TEXT)')
 
 #getting the playlist
 def get_playlist(username, playlist_name):
@@ -47,12 +48,13 @@ def get_tracks(playlist):
         if result:
             continue
         else:
-            cur.execute("INSERT INTO Spotipy (Song_Name, Artist, Length, Spotify_Id) VALUES (?,?,?,?)", (song_name, artist, length, spotify_id))
+            cur.execute("INSERT INTO Spotipy (Song_Name, Artist, Length) VALUES (?,?,?)", (song_name, artist, length))
+            cur.execute('INSERT INTO Spotipy_Ids (Song_Name, Spotify_Id) VALUES(?,?)', (song_name, spotify_id))
             count += 1
             conn.commit()
         if count == 20:
             break           
 
-create_database()
+create_databases()
 get_tracks('US Top 100 SI 206')
 
