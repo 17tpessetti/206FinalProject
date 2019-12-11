21d52143ff3f2e7e7c7d7047b986db634e9fc220
Leila Akan <lakan@umich.edu>
2019-12-11 01:52:07 -0500

    visualization done

--- artist_calculations.py
+++ artist_calculations.py
@@ -2,9 +2,10 @@ import requests
 import json
 import csv
 import sqlite3
-import matplotlib.pyplot as plt
+import matplotlib_venn as vplt
+from matplotlib import pyplot as plt
+
 import os
-import numpy as np
 
 db = 'Music_Data.db'
 path = os.path.dirname(os.path.abspath(__file__))
@@ -23,4 +24,34 @@ def get_favs_from_db():
         w.writerow(['Artist','Total Number of Favorties'])
         w.writerow(["Billie Eilish",total])
 
+
+
+def get_common_song_id():
+    sql = "SELECT * \
+    FROM Spotipy_Ids\
+    LEFT JOIN Hot_100_Musixmatch_Songs\
+    ON Spotipy_Ids.Song_Name = Hot_100_Musixmatch_Songs.Song_Name"
+    cur.execute(sql)
+    cur.execute("SELECT COUNT(Musixmatch_id) FROM Spotipy_Ids")
+    result = cur.fetchone()
+    spotify = 100-result
+    musixmatch = 100-result
+    musixmatch_per = 100-result/100
+    shared = result
+    total_shared = result/100
+    with open('dbcalculation.csv', 'w') as fil:
+        w = csv.writer(fil)
+        w.writerow(['Songs only on Spotify','Songs only on Musixmatch','Shared Songs','percentage'])
+        w.writerow([spotify,musixmatch,shared,total_shared])
+
+
+def make_shared_chart():
+    v = vplt.venn2(subsets={'10': 100, '01': 100, '11': 36}, set_labels = ('A', 'B'),set_colors=('g','b'))
+    v.get_label_by_id('A').set_text('Spotify')
+    v.get_label_by_id('B').set_text('MusixMatch')
+    plt.show()
+
+
 get_favs_from_db() #total number of favorites for artist Billie Eilish
+get_common_song_id()
+make_shared_chart()
